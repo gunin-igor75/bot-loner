7b07020a5e313fc12f07d8533de03d7acaaa3797
igor-gunin75 <igor-gunin75@yandex.ru>
2023-02-24 16:26:06 +0600

    First commit

---
+++ src/main/java/com/example/botloner/BotLonerApplication.java
@@ -0,0 +1,13 @@
+package com.example.botloner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BotLonerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BotLonerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/botloner/BotLonerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.botloner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BotLonerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
